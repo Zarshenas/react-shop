6e3868dc7fcd84d69c5df1ab2fcfbb8da865dc7c
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2023-12-26 07:38:25 +0330

    app root colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ export default {
   darkMode:'class',
   theme: {
     extend: {
+      colors:{
+        'purpleshade-100':'#A685FA',
+        'purpleshade-300':'#8254F8',
+        'purpleshade-400':'#703BF7',
+        'grayshade-50':'#999999',
+        'grayshade-100':'#4D4D4D',
+        'grayshade-200':'#333333',
+        'grayshade-300':'#262626',
+        'grayshade-400':'#1A1A1A',
+        'grayshade-500':'#141414',
+      },
       fontFamily:{
         "urbanist" : ['urbanist', 'sans-serif']
       }
