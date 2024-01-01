0d821612abcde7594c1fb7c4b809356bb56a2011
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2024-01-02 01:13:52 +0330

    header link to "/products" added

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -10,7 +10,9 @@ function Header() {
     <div className="py-4 px-40 relative   w-full">
       <img className='absolute -z-10 top-0 left-0 w-full h-full  object-cover' src="../../public/assets/AbstractDesign.svg" alt="bg-header" />
       <div className='w-full z-99 flex items-center justify-between'>
-      <h1 className='text-5xl font-semibold'>React Shop</h1>
+      <Link to={"/products"}>
+        <h1 className='text-5xl font-semibold'>React Shop</h1>
+      </Link>
       <div className='flex items-center justify-between'>
         <Link className='relative mx-6' to={'/checkout'}>
           <span className='absolute text-sm -top-2 -right-2 p-1 w-5 h-5 text-center leading-none rounded-full bg-purpleshade-400 text-white'>{ordersCount}</span>
