404cca4db6ddea9fe8bb5574b5011f6f6d8567dc
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2024-01-02 12:37:01 +0330

    not found page updated

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,10 @@
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='w-full wrapper flex items-center justify-center flex-col'>
+        <img className='w-96' src="../../public/assets/404.png" alt="emptycart" />
+        <p className='text-3xl font-extrabold'>404</p>
+        <p className='text-lg text-grayshade-100'>Oops! The page you're looking for can't be found.</p>
+    </div>
   )
 }
 
