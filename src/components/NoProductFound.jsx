c249fc602371eaa9030f7353586fade622f7a941
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2024-01-03 08:12:24 +0330

    no product style and text changed

--- src/components/NoProductFound.jsx
+++ src/components/NoProductFound.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function NoProductFound() {
   return (
-    <div className='w-full wrapper flex items-center justify-center flex-col text-center col-start-2 col-end-3'>
+    <div className='w-full flex items-center justify-center flex-col text-center col-start-2 col-end-3 self-start'>
         <img className='w-96' src="../../public/assets/searchnotfound.png" alt="emptycart" />
-        <p className='text-3xl font-extrabold'>404</p>
-        <p className='text-lg text-grayshade-100'>Oops! The page you're looking for can't be found.</p>
+        <p className='text-3xl font-extrabold'>No results found</p>
+        <p className='text-lg text-grayshade-100'>Please try again.</p>
     </div>
   )
 }
