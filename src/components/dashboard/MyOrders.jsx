cab7ea6001b0d3547f4065249bd3e57c55445e58
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2024-01-30 18:38:27 +0330

    my orders completed

--- src/components/dashboard/MyOrders.jsx
+++ src/components/dashboard/MyOrders.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import api from "../../services/axiosConfig";
 import { useAuth } from "../../contexts/AuthenticateProvider";
 import EmptyOrders from "../EmptyOrders";
-import {TailSpin} from 'react-loader-spinner'
+import { TailSpin } from "react-loader-spinner";
 
 const MyOrders = () => {
   const { isAuthenticated } = useAuth();
@@ -24,32 +24,35 @@ const MyOrders = () => {
     <div className="mt-6 h-full items-center">
       {isLoading ? (
         <TailSpin
-        visible={true}
-        height="80"
-        width="80"
-        color="#703BF7"
-        ariaLabel="tail-spin-loading"
-        radius="1"
-        wrapperStyle={{}}
-        wrapperClass=" h-full flex justify-center items-center"
-      />
+          visible={true}
+          height="80"
+          width="80"
+          color="#703BF7"
+          ariaLabel="tail-spin-loading"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=" h-full flex justify-center items-center"
+        />
       ) : !orders.length ? (
         <EmptyOrders />
       ) : (
-        <div className="max-w-md mx-auto bg-gradient-to-r from-teal-400 to-blue-500 text-white rounded-lg shadow-lg p-8 my-8">
-      <h2 className="text-3xl font-extrabold mb-4">Your Order</h2>
-      <p className="mb-2">Total Orders: {orders.ordersCount}</p>
-      <p className="mb-4">Total Price: ${orders.totalPrice}</p>
-
-      <h3 className="text-xl font-bold mb-2">Ordered Products:</h3>
-      <ul>
-        {orders[0].addedProducts.map(product => (
-          <li key={product._id} className="mb-2">
-            <span className="font-semibold">{product.title}</span> - ${product.price} (Quantity: {product.quantity})
-          </li>
-        ))}
-      </ul>
-    </div>
+        orders.map((order) => (
+          <div className="dark:bg-grayshade-500 border border-grayshade-50 dark:border-grayshade-300 rounded-lg my-4 p-8">
+            <ul>
+              {order.addedProducts.map((product) => (
+                <li key={product._id} className="p-2 grid grid-cols-3 border-b border-b-grayshade-50 dark:border-b-grayshade-300">
+                  <span className="font-semibold md:text-lg">{product.title}</span> 
+                  <span className="font-semibold md:text-lg place-self-center"> $ {product.price}</span> 
+                  <span className="font-semibold md:text-lg place-self-center">Quantity: {product.quantity}</span>
+                </li>
+              ))}
+            </ul>
+            <div className="flex justify-evenly mb-0">
+              <p className="font-bold md:text-lg lable">Total Orders : {order.ordersCount}</p>
+              <p className="font-bold md:text-lg lable">Total Price : $ {order.totalPrice} </p>
+            </div>
+          </div>
+        ))
       )}
     </div>
   );
--- src/pages/Dashbord.jsx
+++ src/pages/Dashbord.jsx
@@ -8,7 +8,7 @@ function Dashbord() {
   const { logOut , userInfo: { firstName, lastName } } = useAuth();
   return (
     <div className="wrapper-full bg-lightColor-300 dark:bg-grayshade-300 flex justify-between">
-      <aside className="w-3/12 lg:p-20 bg-lightColor-100 dark:bg-grayshade-500 rounded-xl">
+      <aside className="w-3/12 h-max lg:p-20 bg-lightColor-100 dark:bg-grayshade-500 rounded-xl">
         <ul className="flex flex-col text-3xl [&>li]:list [&>a]:list [&>li]:p-4 [&>a]:p-4">
           <Link to={"my-account"}>
             <TbUser className="mr-2" />
