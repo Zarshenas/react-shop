6a71c759de3914602fa252c45e7323b8931f369a
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2024-02-09 20:52:04 +0330

    baseUrl changed

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // default configurations
-  // baseURL: "https://react-shop-backend.liara.run",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://react-shop-backend.liara.run",
+  // baseURL: "http://localhost:3000",
   withCredentials: true,
 });
 
